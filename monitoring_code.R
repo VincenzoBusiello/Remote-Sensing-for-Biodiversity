68761f4b21000c99e175bd794bae7408bdfec2fd
vincenzobusiello <126822842+VincenzoBusiello@users.noreply.github.com>
2025-05-06 12:48:37 +0200

    Update monitoring_code.R

--- monitoring_code.R
+++ monitoring_code.R
@@ -5,3 +5,11 @@ setwd("C://monitoring")
 mato = rast("matogrosso_l5_1992219.jpg")
 
 plot(mato)
+
+# bands:
+# band 1: near infra red NIR
+# band 2: red
+# band 3: green
+
+plotRGB(mato, r=2, g=3, b=1)
+plotRGB(mato, r=2, g=1, b=3)
